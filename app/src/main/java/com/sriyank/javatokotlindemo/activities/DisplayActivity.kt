7eaeda301c235f3cdb1a9c8b62fb32baf4abd45c
Michael <michael.mansour256@yahoo.com>
2021-09-07 21:13:35 +0200

    something went wrong

--- app/src/main/java/com/sriyank/javatokotlindemo/activities/DisplayActivity.kt
+++ app/src/main/java/com/sriyank/javatokotlindemo/activities/DisplayActivity.kt
@@ -25,105 +25,108 @@ import retrofit2.Response
 import java.util.HashMap
 
 class DisplayActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
     private lateinit var displayAdapter: DisplayAdapter
+
     private var browsedRepositories: List<Repository> = mutableListOf()
+
     private val githubAPIService: GithubAPIService by lazy {
-        RetrofitClient.getGithubAPIService()
+        RetrofitClient.githubAPIService
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
 
         setSupportActionBar(toolbar)
         supportActionBar!!.title = "Showing Browsed Results"
-        setAppUserName()
+
+        setAppUsername()
+
         val layoutManager = LinearLayoutManager(this)
         layoutManager.orientation = LinearLayoutManager.VERTICAL
         recyclerView.layoutManager = layoutManager
 
-
         nav_view.setNavigationItemSelectedListener(this)
 
-        val drawerToggle = ActionBarDrawerToggle(
-            this,
-            drawer_layout,
-            toolbar,
-            R.string.drawer_open,
-            R.string.drawer_close
-        )
+        val drawerToggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.drawer_open, R.string.drawer_close)
         drawer_layout.addDrawerListener(drawerToggle)
         drawerToggle.syncState()
+
         val intent = intent
         if (intent.getIntExtra(Constants.KEY_QUERY_TYPE, -1) == Constants.SEARCH_BY_REPO) {
-            val queryRepo = intent.getStringExtra(Constants.KEY_REPO_SEARCH)
-            val repoLanguage = intent.getStringExtra(Constants.KEY_LANGUAGE)
-            fetchRepositories(queryRepo, repoLanguage!!)
+            val queryRepo = intent.getStringExtra(Constants.KEY_REPO_SEARCH)!!
+            val repoLanguage = intent.getStringExtra(Constants.KEY_LANGUAGE)!!
+            fetchRepositories(queryRepo, repoLanguage)
         } else {
-            val githubUser = intent.getStringExtra(Constants.KEY_GITHUB_USER)
+            val githubUser = intent.getStringExtra(Constants.KEY_GITHUB_USER)!!
             fetchUserRepositories(githubUser)
         }
     }
 
-    private fun setAppUserName() {
-        val sp=getSharedPreferences(Constants.APP_SHARED_PREFERENCES, Context.MODE_PRIVATE)
-        val personName=sp.getString(Constants.KEY_PERSON_NAME,"user")
-        val headerView=nav_view.getHeaderView(0)
-        headerView.txvName.text=personName
+    private fun setAppUsername() {
 
+        val sp = getSharedPreferences(Constants.APP_SHARED_PREFERENCES, Context.MODE_PRIVATE)
+        val personName = sp.getString(Constants.KEY_PERSON_NAME, "User")
+
+        val headerView = nav_view.getHeaderView(0)
+        headerView.txvName.text = personName
     }
 
-    private fun fetchUserRepositories(githubUser: String?) {
+    private fun fetchUserRepositories(githubUser: String) {
 
-        githubAPIService.searchRepositoriesByUser(githubUser).enqueue(object :Callback<List<Repository>>{
-            override fun onResponse(
-                call: Call<List<Repository>>,
-                response: Response<List<Repository>>
-            ) {
-                if (response.isSuccessful){
-                    Log.i(TAG,"posts loaded from api $response")
-                    response.body()?.let{
-                        browsedRepositories=it
+        githubAPIService.searchRepositoriesByUser(githubUser).enqueue(object : Callback<List<Repository>> {
+
+            override fun onResponse(call: Call<List<Repository>>?, response: Response<List<Repository>>) {
+
+                if (response.isSuccessful) {
+                    Log.i(TAG, "posts loaded from API $response")
+
+                    response.body()?.let {
+                        browsedRepositories = it
                     }
-                    if (browsedRepositories.isNotEmpty()){
+
+                    if (browsedRepositories.isNotEmpty()) {
                         setupRecyclerView(browsedRepositories)
-                    }else{
-                        Util.showMessage(this@DisplayActivity,"No items Found")
+                    } else {
+                        Util.showMessage(this@DisplayActivity, "No Items Found")
                     }
-                }else{
-                    Log.i(TAG,"error $response")
-                    Util.showErrorMessage(this@DisplayActivity,response.errorBody())
+
+                } else {
+                    Log.i(TAG, "Error $response")
+                    Util.showErrorMessage(this@DisplayActivity, response.errorBody()!!)
                 }
             }
 
-            override fun onFailure(call: Call<List<Repository>>, t: Throwable) {
-                Util.showMessage(this@DisplayActivity,t.message)
+            override fun onFailure(call: Call<List<Repository>>?, t: Throwable) {
+                Util.showMessage(this@DisplayActivity, t.message)
             }
-
         })
-
-
     }
 
-    private fun fetchRepositories(queryRepo: String?, repoLanguage: String) {
+    private fun fetchRepositories(queryRepo: String, repoLanguage: String) {
         var queryRepo = queryRepo
-        val query: MutableMap<String, String?> = HashMap()
-        if (repoLanguage.isNotEmpty()) queryRepo += " language:$repoLanguage"
+
+        val query = HashMap<String, String>()
+
+        if (repoLanguage.isNotEmpty())
+            queryRepo += " language:$repoLanguage"
         query["q"] = queryRepo
+
         githubAPIService.searchRepositories(query).enqueue(object : Callback<SearchResponse> {
-            override fun onResponse(
-                call: Call<SearchResponse>,
-                response: Response<SearchResponse>
-            ) {
+            override fun onResponse(call: Call<SearchResponse>, response: Response<SearchResponse>) {
                 if (response.isSuccessful) {
                     Log.i(TAG, "posts loaded from API $response")
+
                     response.body()?.items?.let {
-                        browsedRepositories=it
+                        browsedRepositories = it
                     }
 
-                    if (browsedRepositories.isNotEmpty()) setupRecyclerView(browsedRepositories) else Util.showMessage(
-                        this@DisplayActivity,
-                        "No Items Found"
-                    )
+                    if (browsedRepositories.isNotEmpty())
+                        setupRecyclerView(browsedRepositories)
+                    else
+                        Util.showMessage(this@DisplayActivity, "No Items Found")
+
                 } else {
                     Log.i(TAG, "error $response")
                     Util.showErrorMessage(this@DisplayActivity, response.errorBody())
@@ -142,18 +145,23 @@ class DisplayActivity : AppCompatActivity(), NavigationView.OnNavigationItemSele
     }
 
     override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+
         menuItem.isChecked = true
         closeDrawer()
+
         when (menuItem.itemId) {
+
             R.id.item_bookmark -> {
                 showBookmarks()
                 supportActionBar!!.title = "Showing Bookmarks"
             }
+
             R.id.item_browsed_results -> {
                 showBrowsedResults()
                 supportActionBar!!.title = "Showing Browsed Results"
             }
         }
+
         return true
     }
 
@@ -170,10 +178,15 @@ class DisplayActivity : AppCompatActivity(), NavigationView.OnNavigationItemSele
     }
 
     override fun onBackPressed() {
-
+        if (drawer_layout.isDrawerOpen(GravityCompat.START))
+            closeDrawer()
+        else {
+            super.onBackPressed()
+        }
     }
 
     companion object {
+
         private val TAG = DisplayActivity::class.java.simpleName
     }
 }
